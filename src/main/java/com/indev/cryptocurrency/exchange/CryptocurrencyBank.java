40c151ded00ebe8134c70ab643f87efca3132c0b
aboulfadle <mohammedaboulfadle@gmail.com>
2019-02-11 11:44:55 +0100

    application changed

--- src/main/java/com/indev/cryptocurrency/exchange/CryptocurrencyBank.java
+++ src/main/java/com/indev/cryptocurrency/exchange/CryptocurrencyBank.java
@@ -1,14 +1,38 @@
 package com.indev.cryptocurrency.exchange;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CryptocurrencyBank {
-    public void addSupportedCryptoCurrency(String bitcoin) {
-        
+
+    private List<String> supportedCryptoCurrencies = new ArrayList<>();
+    private List<Customer> sellers = new ArrayList<>();
+    private int numberOfBuyers = 0;
+
+
+    public void addSupportedCryptoCurrency(String cryptoCurrency) {
+        supportedCryptoCurrencies.add(cryptoCurrency);
     }
 
-    public int requestTransaction(Customer buyerCustomer, int i, String bitcoin) {
+    public int requestTransaction(Customer buyerCustomer, int numberOfCryptoCurrency, String cryptoCurrency) {
+        if (hasCryptoCurrency(cryptoCurrency)) {
+            Customer sellerCustomer = sellers.remove(0);
+            numberOfBuyers++;
+            int cryptoCurrencyPrice = 1;
+            if(numberOfBuyers > 1)
+                cryptoCurrencyPrice = numberOfBuyers*numberOfBuyers - numberOfBuyers;
+            int boughtQuantity = sellerCustomer.transactionWith(buyerCustomer, numberOfCryptoCurrency, cryptoCurrency, cryptoCurrencyPrice);
+            sellers.add(sellerCustomer);
+            return boughtQuantity;
+        }
         return 0;
     }
 
+    private boolean hasCryptoCurrency(String cryptoCurrency) {
+        return supportedCryptoCurrencies.contains(cryptoCurrency) && !sellers.isEmpty() && sellers.get(0).has(cryptoCurrency);
+    }
+
     public void addSeller(Customer sellerCustomer) {
+        sellers.add(sellerCustomer);
     }
 }
--- src/main/java/com/indev/cryptocurrency/exchange/Customer.java
+++ src/main/java/com/indev/cryptocurrency/exchange/Customer.java
@@ -1,11 +1,56 @@
 package com.indev.cryptocurrency.exchange;
 
 public class Customer {
-    public Customer withCryptocurrency(String bitcoin, int i) {
-        return null;
+
+    private int numberOfCryptoCurrency;
+    private int balence = 0;
+    private String cryptoCurrency;
+    private int cryptoCurrencyPrice = 1;
+
+    public Customer withCryptocurrency(String cryptoCurrency, int numberOfCryptoCurrency) {
+        this.cryptoCurrency = cryptoCurrency;
+        this.numberOfCryptoCurrency = numberOfCryptoCurrency;
+        return this;
     }
 
-    public Customer withBalance(int i) {
-        return null;
+    public String toString() {
+        StringBuilder result = new StringBuilder();
+        result.append(balence).append(":$");
+        if (numberOfCryptoCurrency != 0){
+            result.append(",")
+                    .append(numberOfCryptoCurrency)
+                    .append(":")
+                    .append(cryptoCurrency);
+        }
+        return result.toString();
+    }
+
+    public Customer withBalance(int balence) {
+        this.balence = balence;
+        return this;
+    }
+
+    public int transactionWith(Customer buyerCustomer, int numberOfCryptoCurrency, String cryptoCurrency, int cryptoCurrencyPrice) {
+        addToSeler(numberOfCryptoCurrency,cryptoCurrency,cryptoCurrencyPrice);
+        minusFromBuyer(buyerCustomer, numberOfCryptoCurrency,cryptoCurrency,cryptoCurrencyPrice);
+        return numberOfCryptoCurrency;
+    }
+
+    private void minusFromBuyer(Customer buyerCustomer, int numberOfCryptoCurrency, String cryptoCurrency, int cryptoCurrencyPrice) {
+        buyerCustomer.numberOfCryptoCurrency += numberOfCryptoCurrency;
+        buyerCustomer.balence -= numberOfCryptoCurrency*cryptoCurrencyPrice;
+        buyerCustomer.cryptoCurrency = cryptoCurrency;
+    }
+
+    private void addToSeler(int numberOfCryptoCurrency, String cryptoCurrency, int cryptoCurrencyPrice) {
+        this.numberOfCryptoCurrency -= numberOfCryptoCurrency;
+        this.balence += numberOfCryptoCurrency*cryptoCurrencyPrice;
+        this.cryptoCurrency = cryptoCurrency;
+    }
+
+
+
+    public boolean has(String cryptoCurrency) {
+        return this.cryptoCurrency.equals(cryptoCurrency);
     }
 }
