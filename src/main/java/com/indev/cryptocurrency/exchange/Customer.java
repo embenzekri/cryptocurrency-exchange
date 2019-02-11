8184d87a3dfb24a746b764e1f768ffacb4c59787
Dounia HAMAS <dhamas@sqli.com>
2019-02-11 12:40:44 +0100

    first Commit

--- src/main/java/com/indev/cryptocurrency/exchange/Customer.java
+++ src/main/java/com/indev/cryptocurrency/exchange/Customer.java
@@ -1,11 +1,48 @@
 package com.indev.cryptocurrency.exchange;
 
 public class Customer {
+    private  Currency currency;
+    private int Dollar;
+
+
     public Customer withCryptocurrency(String bitcoin, int i) {
-        return null;
+
+        currency = new Currency(bitcoin,i);
+        return this;
+    }
+
+    public Currency getCurrency() {
+        return currency;
+    }
+
+    public void setCurrencyNumb(int numb) {
+        this.currency.setNumb(numb);
+    }
+
+    public void setCurrency(Currency currency) {
+        this.currency = currency;
+    }
+
+    public int getDollar() {
+        return Dollar;
+    }
+
+    public void setDollar(int dollar) {
+        Dollar = dollar;
+    }
+
+    @Override
+    public String toString() {
+        String result;
+        result = Dollar+":$";
+        if(currency!=null){
+            result +=","+currency.getNumb()+":"+currency.getName();
+        }
+        return result;
     }
 
     public Customer withBalance(int i) {
-        return null;
+        Dollar=i;
+        return this;
     }
 }
