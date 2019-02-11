5cd11d0c3cdf61581f154aa9d6b8397f94803e2b
AKOTBISQLI <47348092+akotbisqli@users.noreply.github.com>
2019-02-11 12:05:01 +0100

    add-crypto: cutrency exchange

--- src/main/java/com/indev/cryptocurrency/exchange/CryptocurrencyBank.java
+++ src/main/java/com/indev/cryptocurrency/exchange/CryptocurrencyBank.java
@@ -1,14 +1,58 @@
 package com.indev.cryptocurrency.exchange;
 
-public class CryptocurrencyBank {
-    public void addSupportedCryptoCurrency(String bitcoin) {
+import java.util.ArrayList;
+import java.util.List;
+
+class CryptocurrencyBank {
+    private List<Customer> sellers;
+    private int buyerRank;
+    CryptocurrencyBank() {
+        this.sellers = new ArrayList<>();
+    }
+
+    void addSupportedCryptoCurrency(String bitcoin) {
         
     }
 
-    public int requestTransaction(Customer buyerCustomer, int i, String bitcoin) {
-        return 0;
+    int requestTransaction(Customer buyerCustomer, int quantity, String cryptoName) {
+        try {
+            int priceInbalance;
+                Customer seller = searchSeller(cryptoName,quantity);
+                priceInbalance = quantity*calculatePrice();
+                updateSeller(seller,quantity,priceInbalance);
+                updateCustomer(buyerCustomer,quantity,cryptoName,priceInbalance);
+                return quantity;
+
+        }catch (Exception e){
+            return 0;
+        }
+
+    }
+    private int calculatePrice(){
+        buyerRank++;
+        if (buyerRank==1)
+            return 1;
+        return buyerRank*buyerRank-buyerRank;
+    }
+    private void updateSeller(Customer seller, int quantity, int priceInbalance){
+        seller.addDolars(priceInbalance);
+        seller.substructCrypto(quantity);
+    }
+    private void updateCustomer(Customer customer, int quantity, String cryptoName, int priceInbalance){
+        customer.addCrypto(quantity,cryptoName);
+        customer.substructDolars(priceInbalance);
+    }
+    private Customer searchSeller(String cryptoName,int quantity) throws Exception {
+        for (Customer sellerCustomer:sellers){
+            if (sellerCustomer.haveTheSameCryptocurrency(cryptoName)&&sellerCustomer.haveEnoughQuantity(quantity))
+                return sellerCustomer;
+        }
+        throw new Exception();
     }
 
-    public void addSeller(Customer sellerCustomer) {
+
+
+    void addSeller(Customer sellerCustomer) {
+        sellers.add(sellerCustomer);
     }
 }
