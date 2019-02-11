d04c7765100de08d8f4dcfde608deb2701ba73a5
Abdelghani <abdelghaniaakri123@gmail.com>
2019-02-11 10:55:26 +0100

    third test passed :p

--- src/main/java/com/indev/cryptocurrency/exchange/Customer.java
+++ src/main/java/com/indev/cryptocurrency/exchange/Customer.java
@@ -2,18 +2,21 @@ package com.indev.cryptocurrency.exchange;
 
 public class Customer {
     private String cryptoCurrency;
+    private int balance;
+
 
     public Customer withCryptocurrency(String bitcoin, int i) {
-        this .cryptoCurrency = bitcoin;
+        this.cryptoCurrency = bitcoin;
         return this;
     }
 
-    public Customer withBalance(int i) {
-        return null;
+    public Customer withBalance(int balance) {
+        this.balance = balance;
+        return this;
     }
 
     @Override
     public String toString() {
-        return "0:$,10:" + cryptoCurrency;
+        return balance +":$,10:" + cryptoCurrency;
     }
 }
