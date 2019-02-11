c4c5ac8cfd746bf9a1e0a6b3ecd193449d91fbcc
AitBassouAli <aitbassouali@gmail.com>
2019-02-11 11:49:16 +0100

    finish the test

--- src/main/java/com/indev/cryptocurrency/exchange/CryptocurrencyBank.java
+++ src/main/java/com/indev/cryptocurrency/exchange/CryptocurrencyBank.java
@@ -1,14 +1,41 @@
 package com.indev.cryptocurrency.exchange;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CryptocurrencyBank {
-    public void addSupportedCryptoCurrency(String bitcoin) {
-        
+    private final List<String> supportedCryptoCurrencies = new ArrayList<>();
+    private List<Customer> sellers = new ArrayList<>();
+    private List<Customer> buyers = new ArrayList<>();
+    private int price = 1;
+
+    public void addSupportedCryptoCurrency(String currency) {
+        supportedCryptoCurrencies.add(currency);
     }
 
-    public int requestTransaction(Customer buyerCustomer, int i, String bitcoin) {
-        return 0;
+    public int requestTransaction(Customer buyerCustomer, int ammount, String currency) {
+        int selledQuantity = 0;
+        if (!sellers.isEmpty()) {
+            for (Customer seller : sellers) {
+                if (seller.hasCurrency(currency)) {
+                    selledQuantity += makeSell(seller, buyerCustomer, ammount, currency);
+                    ammount -= selledQuantity;
+                    if (ammount == 0) break;
+                }
+            }
+        }
+        return selledQuantity;
+    }
+
+    private int makeSell(Customer seller, Customer buyerCustomer, int ammount, String currency) {
+        buyers.add(buyerCustomer);
+        price = buyers.size() == 1 ? 1 : (int) Math.pow(buyers.size(), 2) - buyers.size();
+        seller.selle(ammount, price);
+        buyerCustomer.buy(ammount, currency, price);
+        return ammount;
     }
 
     public void addSeller(Customer sellerCustomer) {
+        sellers.add(sellerCustomer);
     }
 }
