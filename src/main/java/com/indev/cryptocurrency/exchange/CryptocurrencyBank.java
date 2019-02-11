0d5b872f577c4e2004c736cd39cb55322b2feaec
AyoubSqli <47348072+ayoubsqli@users.noreply.github.com>
2019-02-11 15:24:45 +0100

    lAST COMMIT NO SONARLINT ISSUES

--- src/main/java/com/indev/cryptocurrency/exchange/CryptocurrencyBank.java
+++ src/main/java/com/indev/cryptocurrency/exchange/CryptocurrencyBank.java
@@ -5,35 +5,35 @@ import java.util.List;
 
 public class CryptocurrencyBank {
 
-    private List<String> CryptoCurrency = new ArrayList<>() ;private int valueOfCurrency = 1 ;
+    private List<String> cryptoCurrency = new ArrayList<>() ;
+    private int valueOfCurrency = 1 ;
      List<Customer> sellers = new ArrayList<>() ;
     private List<Customer> buyers = new ArrayList<>() ;
     public void addSupportedCryptoCurrency(String bitcoin) {
 
-        CryptoCurrency.add(bitcoin) ;
+        cryptoCurrency.add(bitcoin) ;
     }
-
     public int requestTransaction(Customer customer, int quantite, String cryptoCurrency) {
         customer.setCryptoCurrency(cryptoCurrency);
         buyers.add(customer) ;
         if(isNoSeller()) return 0 ;
-            try {
-                Customer sellerWithEnoughCurrency =  getSellerWithEnoughCurrency(quantite,cryptoCurrency);
-                changeValueOfCryptoCurrency(buyers) ;
-                setChangesToCustomerAndBuyer(sellerWithEnoughCurrency,customer,quantite) ;
-                return quantite ;
-            }catch (Exception e ){
-                return 0 ;
-            }
+                List<Customer> sellerWithEnoughCurrency =  getSellerWithEnoughCurrency(quantite,cryptoCurrency);
+                if(sellerWithEnoughCurrency.isEmpty())
+                {
+                    return 0 ;
+                }else
+                {
+                    changeValueOfCryptoCurrency(buyers) ;
+                    setChangesToCustomerAndBuyer(sellerWithEnoughCurrency.get(0),customer,quantite) ;
+                    return quantite ;
+                }
     }
-
     private void changeValueOfCryptoCurrency(List<Customer> buyers) {
         if(buyers.size() >=2)
         {
             valueOfCurrency = buyers.size()*(buyers.size() - 1) ;
         }
     }
-
     private void setChangesToCustomerAndBuyer(Customer sellerWithEnoughCurrency, Customer customer,int quantite) {
         customer.setAmountCryptoCurrency(customer.getAmountCryptoCurrency()+quantite);
         customer.setAmountDollars(customer.getAmountDollars()-valueOfCurrency*quantite);
@@ -41,17 +41,17 @@ public class CryptocurrencyBank {
         sellerWithEnoughCurrency.setAmountCryptoCurrency(sellerWithEnoughCurrency.getAmountCryptoCurrency()-quantite);
     }
 
-    private Customer getSellerWithEnoughCurrency(int quantite,String cryptoCurrency) throws Exception {
+    private List<Customer> getSellerWithEnoughCurrency(int quantite,String cryptoCurrency) {
+        List<Customer> sellersLigible = new ArrayList<>() ;
         for(Customer seller:sellers){
             if(seller.getAmountCryptoCurrency() >= quantite && seller.getCryptoCurrency().equals(cryptoCurrency)){
-                return seller ;
+                sellersLigible.add(seller);
             }
         }
-         throw new Exception() ;
+         return sellersLigible ;
     }
     private boolean isNoSeller() {
-        if( sellers.size() == 0) return true ;
-        return false;
+        return sellers.isEmpty() ;
     }
     public void addSeller(Customer sellerCustomer) {
         sellers.add(sellerCustomer) ;
