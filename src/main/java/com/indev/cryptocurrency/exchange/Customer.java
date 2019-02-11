7d49ef0a789f9dfd66815d47a13800c419f09aca
KAKDI <khitamakdi@gmail.com>
2019-02-11 14:27:58 +0100

    first commit

--- src/main/java/com/indev/cryptocurrency/exchange/Customer.java
+++ src/main/java/com/indev/cryptocurrency/exchange/Customer.java
@@ -1,11 +1,31 @@
 package com.indev.cryptocurrency.exchange;
 
 public class Customer {
-    public Customer withCryptocurrency(String bitcoin, int i) {
-        return null;
+
+    private String support ;
+    private int balance ;
+    private int price;
+
+    public Customer withCryptocurrency(String support, int price) {
+        price = price;
+        support =support;
+        return this;
     }
 
-    public Customer withBalance(int i) {
-        return null;
+
+    public Customer withBalance(int Balance) {
+        balance=Balance;
+        return this;
     }
+
+    @Override
+    public String toString() {
+        String string =balance+":$";
+        if (price>0)
+        string+= ","+price+":"+ support;
+        return string;
+
+    }
+
+
 }
