141f1ec6d724f276a5705d60eceea0bf37b67322
IELAAZMAOUI <ielaazmaoui@sqli.com>
2019-02-11 13:59:28 +0100

    commit after all tests

--- src/main/java/com/indev/cryptocurrency/exchange/CryptocurrencyBank.java
+++ src/main/java/com/indev/cryptocurrency/exchange/CryptocurrencyBank.java
@@ -1,14 +1,64 @@
 package com.indev.cryptocurrency.exchange;
 
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.List;
+
 public class CryptocurrencyBank {
+    private  int count=1;
+    private  int count2=1;
+  private List supportedCryptoCurrency=new ArrayList<String>();
+ArrayList<Customer> sellers=new ArrayList<Customer>();
     public void addSupportedCryptoCurrency(String bitcoin) {
+        if(!supportedCryptoCurrency.contains(bitcoin))
+        supportedCryptoCurrency.add(bitcoin);
         
     }
 
     public int requestTransaction(Customer buyerCustomer, int i, String bitcoin) {
-        return 0;
+       int j=0;
+
+       Customer  seller=new Customer();
+        Customer  buyer=new Customer();
+        if(sellers.isEmpty())
+        return j;
+        else {
+
+            for (int k=0;k<sellers.size();k++)
+            {
+               if(  sellers.get(k).getCurrencies().containsKey(bitcoin)){
+                   j=i;
+                   seller=sellers.get(k);
+               }
+
+            }
+
+            if(j==i)
+            {
+                 seller.setNbrDollar(i);
+               seller.setCurrencieForSeller(  bitcoin,i,count);
+               count++;
+            }
+
+            for (int k=0;k<sellers.size();k++)
+            {
+                if(  sellers.get(k).getCurrencies().containsKey(bitcoin)){
+                    j=i;
+
+                    buyerCustomer.setCurrencieForBuyer(bitcoin,i,count2);
+                    count2++;
+                    System.out.println("in bank "+buyerCustomer.getNbrDollar());
+
+                }
+
+            }
+
+        }
+        return j;
     }
 
     public void addSeller(Customer sellerCustomer) {
+        sellers.add(sellerCustomer);
     }
 }
--- src/main/java/com/indev/cryptocurrency/exchange/Customer.java
+++ src/main/java/com/indev/cryptocurrency/exchange/Customer.java
@@ -1,11 +1,82 @@
 package com.indev.cryptocurrency.exchange;
 
+import java.util.*;
+
 public class Customer {
+    private  int nbrDollar=0;
+    private Hashtable<String,Integer> currencies;
     public Customer withCryptocurrency(String bitcoin, int i) {
-        return null;
+
+currencies.put(bitcoin,i);
+        System.out.println(currencies.size());
+
+        return this;
     }
 
     public Customer withBalance(int i) {
-        return null;
+   this.nbrDollar=i;
+   return this;
+    }
+
+    @Override
+    public String toString() {
+        String ret=this.nbrDollar+":$";
+        System.out.println("this dol "+this.nbrDollar);
+       Set<String> ta=currencies.keySet();
+        Enumeration e=currencies.keys();
+        String parama;
+        while (e.hasMoreElements()){
+      parama = (String)e.nextElement();
+               ret+=","+currencies.get(parama)+":"+parama ;
+
+      }
+        System.out.println(ret);
+      return ret;
+    }
+
+    public Customer() {
+        this.currencies = new Hashtable<String,Integer> (); ;
+    }
+
+    public int getNbrDollar() {
+        return nbrDollar;
+    }
+
+    public Hashtable<String, Integer> getCurrencies() {
+        return currencies;
+    }
+
+    public void setNbrDollar(int nbrDollar) {
+        this.nbrDollar = nbrDollar;
+    }
+
+    public void setCurrencieForSeller(String bit,int nv,int count) {
+       int au=0;
+        this.nbrDollar=this.nbrDollar*count*count;
+
+        if(currencies.containsKey(bit))
+        {
+
+            au=currencies.get(bit)-nv;
+
+           currencies.remove(bit);
+
+             currencies.put(bit,au);
+
+        }
+    }
+
+    public void setCurrencieForBuyer(String bit,int nv,int count) {
+
+
+        if (count>1)
+        this.withBalance(this.nbrDollar-nv*(count*count-count));
+        else
+            this.withBalance(this.nbrDollar-nv);
+        System.out.println("ds "+this.nbrDollar);
+        currencies.put(bit,nv);
+
+
+
     }
 }
