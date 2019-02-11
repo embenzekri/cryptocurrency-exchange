d17b54bbb4033ad24822ef3d32af5ec0efcd3e4e
AitBassouAli <aitbassouali@gmail.com>
2019-02-11 14:37:48 +0100

    resole reviewing remarks

--- src/main/java/com/indev/cryptocurrency/exchange/CryptocurrencyBank.java
+++ src/main/java/com/indev/cryptocurrency/exchange/CryptocurrencyBank.java
@@ -6,33 +6,39 @@ import java.util.List;
 public class CryptocurrencyBank {
     private final List<String> supportedCryptoCurrencies = new ArrayList<>();
     private List<Customer> sellers = new ArrayList<>();
-    private List<Customer> buyers = new ArrayList<>();
-    private int price = 1;
+    private int buyerRank = 0;
 
     public void addSupportedCryptoCurrency(String currency) {
         supportedCryptoCurrencies.add(currency);
     }
 
     public int requestTransaction(Customer buyerCustomer, int ammount, String currency) {
-        int selledQuantity = 0;
         if (!sellers.isEmpty()) {
-            for (Customer seller : sellers) {
-                if (seller.hasCurrency(currency)) {
-                    selledQuantity += makeSell(seller, buyerCustomer, ammount, currency);
-                    ammount -= selledQuantity;
-                    if (ammount == 0) break;
-                }
+            return doTransaction(buyerCustomer, ammount, currency);
+        }
+        return 0;
+    }
+
+    private int doTransaction(Customer buyerCustomer, int ammount, String currency) {
+        int selledQuantity = 0;
+        for (Customer seller : sellers) {
+            if (seller.hasCurrency(currency)) {
+                selledQuantity += makeSell(seller, buyerCustomer, ammount, currency);
+                ammount -= selledQuantity;
+                if (ammount == 0) break;
             }
         }
         return selledQuantity;
     }
 
     private int makeSell(Customer seller, Customer buyerCustomer, int ammount, String currency) {
-        buyers.add(buyerCustomer);
-        price = buyers.size() == 1 ? 1 : (int) Math.pow(buyers.size(), 2) - buyers.size();
-        seller.selle(ammount, price);
-        buyerCustomer.buy(ammount, currency, price);
+        buyerRank++;
+        seller.selle(ammount, currentPrice());
+        buyerCustomer.buy(ammount, currency, currentPrice());
         return ammount;
+    }
+    private int currentPrice(){
+        return buyerRank == 1 ? 1 : (int) Math.pow(buyerRank, 2) - buyerRank;
     }
 
     public void addSeller(Customer sellerCustomer) {
