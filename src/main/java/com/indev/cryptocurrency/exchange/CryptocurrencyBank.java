d53dea727cd275188ec6eb093670b48807094f80
obennisfeddoul <obennisfeddoul@sqli.com>
2019-02-11 12:22:45 +0100

    All test

--- src/main/java/com/indev/cryptocurrency/exchange/CryptocurrencyBank.java
+++ src/main/java/com/indev/cryptocurrency/exchange/CryptocurrencyBank.java
@@ -1,14 +1,52 @@
 package com.indev.cryptocurrency.exchange;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class CryptocurrencyBank {
-    public void addSupportedCryptoCurrency(String bitcoin) {
-        
+    private ArrayList<String> currencies = new ArrayList<>();
+    private ArrayList<Customer> sellerCustomers = new ArrayList<>();
+    private int numberOfTran =1;
+
+    public void addSupportedCryptoCurrency(String currency) {
+        currencies.add(currency);
     }
 
-    public int requestTransaction(Customer buyerCustomer, int i, String bitcoin) {
+    public int requestTransaction(Customer buyerCustomer, int amount, String currency) {
+        if (numberOfTran == 1){
+            if (sellerCustomers.size() != 0){
+                if(sellerCustomers.get(0).wallet.containsKey(currency)){
+                    sellerCustomers.get(0).withBalance(amount);
+                    sellerCustomers.get(0).withCryptocurrency(currency,sellerCustomers.get(0).wallet.get(currency)-amount);
+
+                    buyerCustomer.withBalance(buyerCustomer.wallet.get("$")-amount);
+                    buyerCustomer.withCryptocurrency(currency,amount);
+
+                    numberOfTran++;
+                    return amount;
+                }
+                return 0;
+            }
+        }else {
+            if (sellerCustomers.size() != 0){
+                if(sellerCustomers.get(0).wallet.containsKey(currency)){
+                    sellerCustomers.get(0).withBalance(sellerCustomers.get(0).wallet.get("$")+(int)(Math.pow(numberOfTran,2)-numberOfTran)*amount);
+                    sellerCustomers.get(0).withCryptocurrency(currency,sellerCustomers.get(0).wallet.get(currency)-amount);
+
+                    buyerCustomer.withBalance(buyerCustomer.wallet.get("$")-((int)(Math.pow(numberOfTran,2)-numberOfTran)*amount));
+                    buyerCustomer.withCryptocurrency(currency,amount);
+
+                    numberOfTran++;
+                    return amount;
+                }
+                return 0;
+            }
+        }
+
         return 0;
     }
 
     public void addSeller(Customer sellerCustomer) {
+        sellerCustomers.add(sellerCustomer);
     }
 }
--- src/main/java/com/indev/cryptocurrency/exchange/Customer.java
+++ src/main/java/com/indev/cryptocurrency/exchange/Customer.java
@@ -1,11 +1,37 @@
 package com.indev.cryptocurrency.exchange;
 
+import java.util.HashMap;
+
 public class Customer {
-    public Customer withCryptocurrency(String bitcoin, int i) {
-        return null;
+
+    public HashMap<String,Integer> wallet = new HashMap<>();
+    private String balance = "";
+    private int k=0;
+
+    public Customer() {
+        wallet.put("$",0);
     }
 
-    public Customer withBalance(int i) {
-        return null;
+    public Customer withCryptocurrency(String bitcoin, int amount) {
+        wallet.put(bitcoin,amount);
+        return this;
+    }
+
+    public Customer withBalance(int amount) {
+        wallet.put("$",amount);
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        k=wallet.size();
+        for (HashMap.Entry<String, Integer> entry : wallet.entrySet()) {
+            balance+=entry.getValue()+":"+entry.getKey();
+            if (k>1){
+                balance+=",";
+            }
+            k--;
+        }
+        return balance;
     }
 }
