e8d6c7c6a0dd0293d09f84d633395c44c7ed36f5
ablil <ablil@protonmail.com>
2021-09-20 14:58:21 +0100

    all test passed

--- src/main/java/com/indev/cryptocurrency/exchange/CryptocurrencyBank.java
+++ src/main/java/com/indev/cryptocurrency/exchange/CryptocurrencyBank.java
@@ -1,14 +1,38 @@
 package com.indev.cryptocurrency.exchange;
 
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+
 public class CryptocurrencyBank {
+
+    private final Set<String> currencies = new HashSet<>();
+    private final Set<Customer> sellers = new HashSet<>();
+    private int buyingCounter = 0;
+
     public void addSupportedCryptoCurrency(String bitcoin) {
-        
+        currencies.add(bitcoin);
     }
 
     public int requestTransaction(Customer buyerCustomer, int i, String bitcoin) {
+        Optional<Customer> seller =
+                sellers.stream().filter(s -> s.getCurrency().equals(bitcoin)).findFirst();
+        if (seller.isPresent()) {
+            buyingCounter++;
+            int price = calcPrice();
+            seller.get().sell(i, price);
+            buyerCustomer.buy(i, price, bitcoin);
+            return i;
+        }
         return 0;
     }
 
+    private int calcPrice() {
+        return  buyingCounter > 1 ?
+                buyingCounter * buyingCounter - buyingCounter : 1;
+    }
+
     public void addSeller(Customer sellerCustomer) {
+        sellers.add(sellerCustomer);
     }
 }
--- src/main/java/com/indev/cryptocurrency/exchange/Customer.java
+++ src/main/java/com/indev/cryptocurrency/exchange/Customer.java
@@ -1,11 +1,50 @@
 package com.indev.cryptocurrency.exchange;
 
 public class Customer {
+    
+    private String currency;
+    private int quantity = 0;
+    private int balance = 0;
+
+    public Customer() {
+        this.currency = "";
+    }
+
+    @Override
+    public String toString() {
+        if (quantity != 0) {
+            return String.format("%s:$,%s:%s", balance, quantity, currency);
+        } else {
+            return String.format("%s:$", balance);
+        }
+    }
+
+    public Customer(String currency, int quantity, int balance) {
+        this.currency = currency;
+        this.quantity = quantity;
+        this.balance = balance;
+    }
+
     public Customer withCryptocurrency(String bitcoin, int i) {
-        return null;
+        return new Customer(bitcoin, i, balance);
     }
 
     public Customer withBalance(int i) {
-        return null;
+        return new Customer(currency, quantity, i);
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void sell(int quantity, int unitPrice) {
+        this.quantity -= quantity;
+        this.balance += unitPrice * quantity;
+    }
+
+    public void buy(int quantity, int unitPrice, String currency) {
+        this.quantity += quantity;
+        this.balance -= unitPrice * quantity;
+        this.currency = currency;
     }
 }
